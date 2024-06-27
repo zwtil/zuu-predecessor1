b16cd9d8783fca39fc57aa69a78b4df03d229f93
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-06-27 08:44:05 -0700

    feat(tool): sysinfo

---
+++ src/zrcl/tool_sysinfo.py
@@ -0,0 +1,18 @@
+import platform
+import psutil
+
+def run():
+    # sysinfo
+    # Operating System Information
+    print("Operating System:", platform.system())
+    print("OS Version:", platform.version())
+    print("OS Release:", platform.release())
+
+    # CPU Information
+    print("CPU Cores:", psutil.cpu_count(logical=False))
+    print("Total CPU Threads:", psutil.cpu_count(logical=True))
+
+    # Memory Information
+    memory = psutil.virtual_memory()
+    print("Total Memory:", memory.total)
+    print("Available Memory:", memory.available)
